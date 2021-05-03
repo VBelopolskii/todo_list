dfb0f46fc9e1a32db2b13bd2b910c99c02429654
valerii.belopolskii <valerii.belopolskii@crplab.ru>
2021-05-03 09:19:47 +0300

    test: selected task on click

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 import {TASKS} from "./tasks.js"
 
 const tasksList = document.querySelector('.content__main-list');
+const tasks = document.querySelectorAll('.content__main-list li');
 const counterElement = document.querySelector('.task-count');
 const inputNewTask = document.getElementById("footer__new-task");
-const addNewTaskBtn = document.querySelector(".footer__button");
 const addNewTaskForm = document.querySelector(".footer__form");
 
 const TASK_CSS_CLASS = {
@@ -21,17 +21,22 @@ function generateTemplate(task) {
     tasksList.append(taskListEl);
     taskInput.type = 'checkbox';
     taskListEl.className = task.isChecked ? TASK_CSS_CLASS.CHECKED : TASK_CSS_CLASS.UNCHECKED;
-}
-
-function setTasksCounter() {
-    counterElement.innerText = TASKS.length;
+    taskListEl.id = task.id;
+    taskInput.className = taskListEl.className;
 }
 
 function renderTasks() {
     TASKS.forEach(task => {
         generateTemplate(task);
+        if (task.isChecked) {
+            putTick(task);
+        }
     });
     setTasksCounter();
+}
+
+function setTasksCounter() {
+    counterElement.innerText = TASKS.length;
 }
 
 function renderNewTask() {
@@ -50,22 +55,68 @@ function addNewTask(evt) {
 
     TASKS.push(
         {
-            id: TASKS.length,
+            id: TASKS.length + 1,
             name: inputNewTask.value.trim(),
             isChecked: false
         });
 
     renderNewTask();
-    clearInputField();
+    clearInput();
 
 }
 
-function clearInputField() {
+function clearInput() {
     inputNewTask.value = "";
 }
 
 
 addNewTaskForm.addEventListener("submit", addNewTask);
+
+
+
+// tasks.addEventListener("click", task => {selectTask(task)});
+
+// tasks.forEach(function(task) {
+//     task.addEventListener("click", selectTask)
+// })
+
+function selectTask(event) {
+    const task = event.target;
+    if(task.tagName === "LI") {
+        if (task.classList.contains(TASK_CSS_CLASS.CHECKED)) {
+            task.classList.remove(TASK_CSS_CLASS.CHECKED);
+            task.classList.add(TASK_CSS_CLASS.UNCHECKED);
+        } else {
+            task.classList.remove(TASK_CSS_CLASS.UNCHECKED);
+            task.classList.add(TASK_CSS_CLASS.CHECKED);
+        }
+        putTick(task);
+        console.log(TASKS[task.id - 1]);
+    }
+};
+
+tasksList.addEventListener("click", selectTask);
+
+
+//------------------------------------------
+function isCheckboxChecked(taskEl) {
+    return document.querySelector(`li[id="${taskEl.id}"] input`).checked;
+}
+
+function changeCheckboxProp(task, taskEl) {
+    if (task.isChecked === TASK_CSS_CLASS.CHECKED) {
+        taskEl.querySelector("input").checked = true;
+    } else {
+        taskEl.querySelector("input").checked = false;
+    }
+}
+
+function putTick(task) {
+    document.querySelector(`li[id="${task.id}"] input`).click();
+}
+
+
+//--------------------------------------------
 
 function init() {
     renderTasks();
